81eacf7e0151833d9feedfa92a9ceaa036e0873e
alex-slobodian <alexx.slobodyan@gmail.com>
2024-12-06 18:20:11 +0200

    Init

---
+++ README.md
@@ -0,0 +1,113 @@
+# Go Simple CRUD Application with MySql and Gin Framework
+
+This is a simple CRUD (Create, Read, Update, Delete) application built in Go using the [Gin Web Framework](https://github.com/gin-gonic/gin) and MySql. The application manages a list of `todos`, allowing users to create, read, update, and delete `todo` items.
+
+## Features
+
+- **Create a Todo**: Add a new `todo` item with a ID, description, and completion status.
+- **Read Todos**: Retrieve the list of all `todos` or get details for a specific `todo`.
+- **Update a Todo**: Edit an existing `todo` by updating its description and/or completion status.
+- **Delete a Todo**: Remove a `todo` item from the list.
+
+## Endpoints
+
+- `GET /todos` - Retrieves the full list of todos.
+- `POST /todos` - Creates a new todo item.
+- `GET /todos/:id` - Retrieves details of a specific todo by ID.
+- `PATCH /todos/:id` - Toggles the completion status of a todo.
+- `PUT /todos/:id` - Updates the description and/or completion status of a specific todo.
+- `DELETE /todos/:id` - Deletes a specific todo by ID.
+
+## Quick Start
+
+### Prerequisites
+
+- [Go](https://golang.org/dl/) (version 1.23.3 or higher)
+- [Gin Web Framework](https://github.com/gin-gonic/gin)
+- [MySql](https://www.mysql.com/)
+- [Docker](https://docs.docker.com/get-docker/)
+
+### Installation
+
+1. **Clone the repository**:
+
+   ```bash
+   git clone https://github.com/aleksandr-slobodian/go-simple-crud-mysql
+   cd go-simple-crud-mysql
+   ```
+
+2. **Install dependencies**:
+
+   ```bash
+   go mod download
+   ```
+
+3. **Run the MySql server**:
+
+   ```bash
+   docker-compose up -d
+   ```
+
+4. **Run the migration**:
+
+   Install [migrate](https://github.com/golang-migrate/migrate) and run the following command:
+
+   ```bash
+   make migrate-up
+   ```
+
+5. **Run the application**:
+
+   with go:
+
+   ```bash
+   go run main.go
+   ```
+
+   with [Air - Live reload](https://github.com/air-verse/air):
+
+   ```bash
+   air
+   ```
+
+### Usage
+
+1. **Create a new todo**:
+
+   ```bash
+   curl -X POST -H "Content-Type: application/json" -d '{"item": "Buy groceries", "completed": false}' http://localhost:9191/todos
+   ```
+
+2. **Retrieve all todos**:
+
+   ```bash
+   curl http://localhost:9191/todos
+   ```
+
+3. **Retrieve a specific todo**:
+
+   ```bash
+   curl http://localhost:9191/todos/1
+   ```
+
+4. **Update a todo**:
+
+   ```bash
+   curl -X PUT -H "Content-Type: application/json" -d '{"item": "Buy groceries", "completed": true}' http://localhost:9191/todos/1
+   ```
+
+5. **Update todo's completed status**:
+
+   ```bash
+   curl -X PUTCH http://localhost:9191/todos/1
+   ```
+
+6. **Delete a todo**:
+
+   ```bash
+   curl -X DELETE http://localhost:9191/todos/1
+   ```
+
+## License
+
+This project is licensed under the MIT License.
---
+++ main.go
@@ -0,0 +1,239 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var (
+	db       *sql.DB
+	Validate *validator.Validate
+)
+
+type todo struct {
+	ID        int    `json:"id"`
+	Item      string `json:"item"`
+	Completed bool   `json:"completed"`
+}
+
+func init() {
+	Validate = validator.New()
+}
+
+func parseValidationError(err error) string {
+	if validationErrors, ok := err.(validator.ValidationErrors); ok {
+		var result string
+		for _, fieldError := range validationErrors {
+			result += fmt.Sprintf(
+				"Field validation for '%s' failed: '%s' (condition: %s)\n",
+				fieldError.Field(),
+				fieldError.ActualTag(),
+				fieldError.Param(),
+			)
+		}
+		return result
+	}
+	return "an unknown validation error occurred"
+}
+
+func parseIDParam(ginContext *gin.Context) (int64, error) {
+	idParam := ginContext.Param("id")
+	id, err := strconv.ParseInt(idParam, 10, 64)
+	if err != nil {
+		return 0, fmt.Errorf("invalid id format")
+	}
+	return id, nil
+}
+
+type todoPayload struct {
+	Item      string `json:"item" validate:"required,max=100,min=2"`
+	Completed bool   `json:"completed"`
+}
+
+func createTodo(ginContext *gin.Context) {
+	var payload todoPayload
+
+	if err := ginContext.ShouldBindJSON(&payload); err != nil {
+		ginContext.JSON(http.StatusBadRequest, gin.H{"error": "invalid request body"})
+		return
+	}
+
+	if err := Validate.Struct(payload); err != nil {
+		ginContext.JSON(http.StatusBadRequest, gin.H{"error": parseValidationError(err)})
+		return
+	}
+
+	result, err := db.Exec("INSERT INTO todos (item, completed) VALUES (?, ?)", payload.Item, payload.Completed)
+	if err != nil {
+		ginContext.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	id, _ := result.LastInsertId()
+	ginContext.JSON(http.StatusCreated, gin.H{"id": id, "item": payload.Item, "completed": payload.Completed})
+}
+
+func getTodos(ginContext *gin.Context) {
+rows, err := db.Query("SELECT id, item, completed FROM todos")
+	if err != nil {
+		ginContext.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	defer rows.Close()
+
+	var todos = []todo{}
+	for rows.Next() {
+		var t todo
+		if err := rows.Scan(&t.ID, &t.Item, &t.Completed); err != nil {
+			ginContext.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		todos = append(todos, t)
+	}
+
+	ginContext.JSON(http.StatusOK, todos)
+}
+
+func getTodo(ginContext *gin.Context) {
+	id, err := parseIDParam(ginContext)
+	if err != nil {
+		ginContext.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	var todo todo
+	err = db.QueryRow("SELECT id, item, completed FROM todos WHERE id = ?", id).Scan(
+		&todo.ID, &todo.Item, &todo.Completed,
+	)
+
+	if err == sql.ErrNoRows {
+		ginContext.JSON(http.StatusNotFound, gin.H{"error": "todo not found"})
+		return
+	} else if err != nil {
+		ginContext.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	ginContext.JSON(http.StatusOK, todo)
+}
+
+func toggleTodoStatus(ginContext *gin.Context) {
+	id, err := parseIDParam(ginContext)
+	if err != nil {
+		ginContext.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	var todo todo
+	err = db.QueryRow("SELECT id, item, completed FROM todos WHERE id = ?", id).Scan(
+		&todo.ID, &todo.Item, &todo.Completed,
+	)
+
+	if err == sql.ErrNoRows {
+		ginContext.JSON(http.StatusNotFound, gin.H{"error": "todo not found"})
+		return
+	} else if err != nil {
+		ginContext.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	newStatus := !todo.Completed
+	_, err = db.Exec("UPDATE todos SET completed = ? WHERE id = ?", newStatus, id)
+	if err != nil {
+		ginContext.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	ginContext.JSON(http.StatusOK, gin.H{"id": todo.ID, "item": todo.Item, "completed": newStatus})
+}
+
+func updateTodo(ginContext *gin.Context) {
+	id, err := parseIDParam(ginContext)
+	if err != nil {
+		ginContext.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	var payload todoPayload
+	if err := ginContext.ShouldBindJSON(&payload); err != nil {
+		ginContext.JSON(http.StatusBadRequest, gin.H{"error": "invalid request body"})
+		return
+	}
+
+	if err := Validate.Struct(payload); err != nil {
+		ginContext.JSON(http.StatusBadRequest, gin.H{"error": parseValidationError(err)})
+		return
+	}
+
+	result, err := db.Exec("UPDATE todos SET item = ?, completed = ? WHERE id = ?", payload.Item, payload.Completed, id)
+	if err != nil {
+		ginContext.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	rowsAffected, _ := result.RowsAffected()
+	if rowsAffected == 0 {
+		ginContext.JSON(http.StatusNotFound, gin.H{"error": "todo not found"})
+		return
+	}
+
+	ginContext.JSON(http.StatusOK, gin.H{"id": id, "item": payload.Item, "completed": payload.Completed})
+}
+
+func deleteTodo(ginContext *gin.Context) {
+	id, err := parseIDParam(ginContext)
+	if err != nil {
+		ginContext.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	var deletedTodo todo
+	err = db.QueryRow("SELECT id, item, completed FROM todos WHERE id = ?", id).Scan(
+		&deletedTodo.ID, &deletedTodo.Item, &deletedTodo.Completed,
+	)
+	if err == sql.ErrNoRows {
+		ginContext.JSON(http.StatusNotFound, gin.H{"error": "todo not found"})
+		return
+	} else if err != nil {
+		ginContext.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	_, err = db.Exec("DELETE FROM todos WHERE id = ?", id)
+	if err != nil {
+		ginContext.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	ginContext.IndentedJSON(http.StatusOK, deletedTodo)
+}
+
+func main() {
+	var err error
+	db, err = sql.Open("mysql", "admin:adminpassword@tcp(localhost:3306)/app_db")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	if err := db.Ping(); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Connected to MySQL")
+
+	router := gin.Default()
+	router.GET("/todos", getTodos)
+	router.POST("/todos", createTodo)
+	router.GET("/todos/:id", getTodo)
+	router.PATCH("/todos/:id", toggleTodoStatus)
+	router.PUT("/todos/:id", updateTodo)
+	router.DELETE("/todos/:id", deleteTodo)
+	router.Run("localhost:9191")
+}
